d286ea70aea9bf1eacdbfb358a74c61b3e5e12a6
Tom Softreck <tom@sapletta.com>
2026-03-01 11:16:05 +0100

    refactoring

--- project.sh
+++ project.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 pip install code2logic --upgrade
-pip install code2flow --upgrade
+pip install code2llm --upgrade
 
 #code2logic ./ -f toon --compact --no-repeat-module --function-logic --with-schema --name project -o ./
 
 source venv/bin/activate && python -m code2logic ./ -f toon --compact --name project -o ./
-source venv/bin/activate && python -m code2flow ./ -v -o ./project -m hybrid -f toon
+#source venv/bin/activate && python -m code2llm ./ -v -o ./project -m hybrid -f toon
+source venv/bin/activate && python -m code2llm -f all
+#code2llm ./ -f all
